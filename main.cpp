29527ab1e4712785155247724d5e292fec8d1a9e
cppwithEmelyne <ihirweemelyne1@gmail.com>
2026-05-14 14:07:12 +0200

    fibonacci series

--- main.cpp
+++ main.cpp
@@ -1,26 +1,27 @@
 #include <iostream>
 
 using namespace std;
+void fibonacie(int n){
+int a=0;
+int b=1;
+int c;
+cout<<a<<b<<endl;
+for(int i=1;i<=n-2;i++)
+                {
 
 
-float add(float a,float b,float c,float d)
-{
-return a+b+c+d;
+c=a+b;
+cout<<c<<endl;
+a=b;
+b=c;}
 }
-int main(){
-float n1;
-float n2;
-float n3;
-float n4;
-float sum;
-cout<<"enter 4 numbers:";
-cin>>n1;
-cin>>n2;
-cin>>n3;
-cin>>n4;
-sum=add(n1,n2,n3,n4);
-cout<<"sum is:"<<sum<<endl;
 
+
+int main()
+{int n;
+cout<<"enter number of terms";
+cin>>n;
+fibonacie(n);
 
     return 0;
 }
