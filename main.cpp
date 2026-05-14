12d17a6733308a7d3d4372df186a4325a4dbf64b
cppwithEmelyne <ihirweemelyne1@gmail.com>
2026-05-14 14:00:02 +0200

    congratulation

--- main.cpp
+++ main.cpp
@@ -1,27 +1,20 @@
 #include <iostream>
-#include <string>
+
 using namespace std;
 
-int main(){
+void congratulate(string name)
+{
+    cout << "Congratulations " << name << endl;
+}
 
-int matrix[3 ] [2];
-   int i,j;
-   cout<<"enter number in matrix";
-   for(i=0;i<3;i++){
-   for (j=0;j<2;j++){
-   cin>> matrix[i][j];
-   }
-   }
-   cout<< "matrix is";
-   for (i=0;i<3;i++){
-   for (j=0;j<2;j++){
-   cout<<matrix[i][j]<<" ";
-   }
-   cout<<endl;
-   }
+int main()
+{
+    string name;
 
+    cout << "Enter your name: ";
+    cin >> name;
+
+    congratulate(name);
 
     return 0;
 }
-
-
